8383837b9508b978e3197399d7cc6d4afefcadf2
조현준 <aaiss0927@naver.com>
2024-10-24 13:02:55 +0900

    delete ensemble code

--- HZ_ensemble/transform_csv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-pred_df = pd.read_csv('/data/ephemeral/home/baseline/HZ_ensemble/val_inf/dino_val_inf.csv')
-
-pred_df['image_id'] = "train/" + pred_df['image_id']
-pred_df.to_csv('real.csv')
