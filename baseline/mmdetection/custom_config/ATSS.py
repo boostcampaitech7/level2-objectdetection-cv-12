210ce5dace1b258170c79f57e2b93011a45d5567
Hwan7919 <hwan7919@gmail.com>
2024-10-24 11:08:33 +0900

    Comment : custom configs

--- baseline/mmdetection/custom_config/ATSS.py
+++ baseline/mmdetection/custom_config/ATSS.py
@@ -3,17 +3,19 @@ _base_ = [
     '../configs/_base_/default_runtime.py'
 ]
 
-# model
+# 모델 설정
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/dyhead/atss_swin-l-p4-w12_fpn_dyhead_mstrain_2x_coco/atss_swin-l-p4-w12_fpn_dyhead_mstrain_2x_coco_20220509_100315-bc5b6516.pth'
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # noqa
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # 사전 학습된 가중치 링크
+
 model = dict(
     type='ATSS',
     data_preprocessor=dict(
         type='DetDataPreprocessor',
-        mean=[123.675, 116.28, 103.53],
-        std=[58.395, 57.12, 57.375],
-        bgr_to_rgb=True,
-        pad_size_divisor=128),
+        mean=[123.675, 116.28, 103.53],  # 평균값
+        std=[58.395, 57.12, 57.375],     # 표준편차
+        bgr_to_rgb=True,                 # BGR을 RGB로 변환
+        pad_size_divisor=128              # 패딩 크기 배수
+    ),
     backbone=dict(
         type='SwinTransformer',
         pretrain_img_size=384,
@@ -24,16 +26,15 @@ model = dict(
         mlp_ratio=4,
         qkv_bias=True,
         qk_scale=None,
-        drop_rate=0.,
-        attn_drop_rate=0.,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
         drop_path_rate=0.2,
         patch_norm=True,
-        out_indices=(1, 2, 3),
-        # Please only add indices that would be used
-        # in FPN, otherwise some parameter will not be used
+        out_indices=(1, 2, 3),  # FPN에서 사용할 출력 인덱스
         with_cp=False,
         convert_weights=True,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)  # 사전 학습된 가중치 로드
+    ),
     neck=[
         dict(
             type='FPN',
@@ -41,20 +42,21 @@ model = dict(
             out_channels=256,
             start_level=0,
             add_extra_convs='on_output',
-            num_outs=5),
+            num_outs=5
+        ),
         dict(
             type='DyHead',
             in_channels=256,
             out_channels=256,
             num_blocks=6,
-            # disable zero_init_offset to follow official implementation
-            zero_init_offset=False)
+            zero_init_offset=False  # 공식 구현을 따름
+        )
     ],
     bbox_head=dict(
         type='ATSSHead',
-        num_classes=10,
+        num_classes=10,  # 클래스 수 설정
         in_channels=256,
-        pred_kernel_size=1,  # follow DyHead official implementation
+        pred_kernel_size=1,  # DyHead 공식 구현을 따름
         stacked_convs=0,
         feat_channels=256,
         anchor_generator=dict(
@@ -63,52 +65,63 @@ model = dict(
             octave_base_scale=8,
             scales_per_octave=1,
             strides=[8, 16, 32, 64, 128],
-            center_offset=0.5),  # follow DyHead official implementation
+            center_offset=0.5  # DyHead 공식 구현을 따름
+        ),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
-            target_means=[.0, .0, .0, .0],
-            target_stds=[0.1, 0.1, 0.2, 0.2]),
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[0.1, 0.1, 0.2, 0.2]
+        ),
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
-            loss_weight=1.5),
+            loss_weight=1.5
+        ),
         loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
         loss_centerness=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
-    # training and testing settings
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0
+        )
+    ),
+    # 학습 및 테스트 설정
     train_cfg=dict(
         assigner=dict(type='ATSSAssigner', topk=9),
         allowed_border=-1,
         pos_weight=-1,
-        debug=False),
+        debug=False
+    ),
     test_cfg=dict(
         nms_pre=1000,
         min_bbox_size=0,
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.5),
-        max_per_img=100))
-
-# optimizer
-optim_wrapper = dict(
-    _delete_=True,
-    type='AmpOptimWrapper',
-    optimizer=dict(
-        type='AdamW', lr=0.00005, betas=(0.9, 0.999), weight_decay=0.05),
-    paramwise_cfg=dict(
-        custom_keys={
-            'absolute_pos_embed': dict(decay_mult=0.),
-            'relative_position_bias_table': dict(decay_mult=0.),
-            'norm': dict(decay_mult=0.)
-        }),
-    clip_grad=None
+        max_per_img=100
+    )
 )
 
-# amp
-optim_wrapper = dict(type='AmpOptimWrapper', loss_scale = 'dynamic')
+# 옵티마이저 및 AMP 설정
+optim_wrapper = dict(
+    _delete_=True,  # 기존 설정 삭제
+    type='AmpOptimWrapper',  # 자동 혼합 정밀도 사용
+    optimizer=dict(
+        type='AdamW',
+        lr=0.00005,
+        betas=(0.9, 0.999),
+        weight_decay=0.05
+    ),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.0),
+            'relative_position_bias_table': dict(decay_mult=0.0),
+            'norm': dict(decay_mult=0.0)
+        }
+    ),
+    clip_grad=None,
+    loss_scale='dynamic'  # 동적 손실 스케일링
+)
 
-# learning rate
+# 학습률 스케줄러 설정
 param_scheduler = [
     dict(
         type='CosineAnnealingLR',
@@ -120,33 +133,47 @@ param_scheduler = [
     )
 ]
 
-
-### wandb ###
+### WandB 시각화 설정 ###
 vis_backends = [
     dict(type='LocalVisBackend'),
-    dict(type='WandbVisBackend',
-         init_kwargs={
+    dict(
+        type='WandbVisBackend',
+        init_kwargs={
             'project': 'DINO',
             'entity': 'yujihwan-yonsei-university',
-            'name': 'DINO_NEWFOLD_12EPOCH'  # ex) swin-l_5scale_original_randaug_epochs6 형식으로 변경 해줄 것!
-         })]
+            'name': 'DINO_NEWFOLD_12EPOCH'  # 예: swin-l_5scale_original_randaug_epochs6 형식으로 변경
+        }
+    )
+]
 
-visualizer = dict(type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=vis_backends,
+    name='visualizer'
+)
 
-# logger val
+# 로깅 및 체크포인트 설정
 default_hooks = dict(
-    checkpoint=dict(type='CheckpointHook', interval=1, save_best='auto', max_keep_ckpts=3))
+    checkpoint=dict(
+        type='CheckpointHook',
+        interval=1,                # 체크포인트 저장 주기 (epoch 단위)
+        save_best='auto',          # 자동으로 베스트 모델 저장
+        max_keep_ckpts=3           # 최대 저장할 체크포인트 수
+    )
+)
 
-workflow = [('train', 1), ('val', 1)]
+workflow = [('train', 1), ('val', 1)]  # 학습과 검증을 번갈아 수행
 
-# custom_hooks = [
-#     dict(type='SubmissionHook')
-# ]
-
+# Test Time Augmentation (TTA) 모델 설정
 tta_model = dict(
     type='DetTTAModel',
-    tta_cfg=dict(nms=dict(type='nms', iou_threshold=0.5), max_per_img=300))
-img_scales=[(2000, 480), (2000, 1200)]
+    tta_cfg=dict(
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=300
+    )
+)
+
+img_scales = [(2000, 480), (2000, 1200)]  # TTA를 위한 이미지 스케일
 tta_pipeline = [ 
     dict(type='LoadImageFromFile', backend_args=None),
     dict(
@@ -157,28 +184,34 @@ tta_pipeline = [
                 for s in img_scales
             ],
             [
-                # ``RandomFlip`` must be placed before ``Pad``, otherwise
-                # bounding box coordinates after flipping cannot be
-                # recovered correctly.
-                dict(type='RandomFlip', prob=1.),
-                dict(type='RandomFlip', prob=0.)
+                # `RandomFlip`은 `Pad` 전에 배치해야 플립 후 바운딩 박스 좌표를 올바르게 복구할 수 있음
+                dict(type='RandomFlip', prob=1.0),
+                dict(type='RandomFlip', prob=0.0)
             ],
             [dict(type='LoadAnnotations', with_bbox=True)],
             [
                 dict(
                     type='PackDetInputs',
-                    meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-                               'scale_factor', 'flip', 'flip_direction'))
+                    meta_keys=(
+                        'img_id', 'img_path', 'ori_shape', 'img_shape',
+                        'scale_factor', 'flip', 'flip_direction'
+                    )
+                )
             ]
-        ])
+        ]
+    )
 ]
 
-# dataset settings
+# 데이터셋 설정
 dataset_type = 'CocoDataset'
 data_root = '/data/ephemeral/home/level2-objectdetection-cv-12/FOLD'
-classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
-            "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+classes = (
+    "General trash", "Paper", "Paper pack", "Metal", "Glass", 
+    "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing"
+)
 backend_args = None
+
+# 학습 파이프라인
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
     dict(type='LoadAnnotations', with_bbox=True),
@@ -186,21 +219,40 @@ train_pipeline = [
         type='RandomResize',
         scale=[(2000, 480), (2000, 1200)],
         keep_ratio=True,
-        backend='pillow'),
+        backend='pillow'
+    ),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PackDetInputs')
 ]
+
+# 검증 파이프라인
 val_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='Resize', scale=(2000, 1200), keep_ratio=True, backend='pillow'),
+    dict(
+        type='Resize',
+        scale=(2000, 1200),
+        keep_ratio=True,
+        backend='pillow'
+    ),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='PackDetInputs',
-        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+        meta_keys=(
+            'img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'
+        )
+    )
 ]
+
+# 테스트 파이프라인
 test_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
-    dict(type='Resize', scale=(2000, 1200), keep_ratio=True, backend='pillow'),
+    dict(
+        type='Resize',
+        scale=(2000, 1200),
+        keep_ratio=True,
+        backend='pillow'
+    ),
+    # Test Time Augmentation 주석 처리됨
     # dict(
     #     type='TestTimeAug',
     #     transforms=[
@@ -209,8 +261,13 @@ test_pipeline = [
     # ),
     dict(
         type='PackDetInputs',
-        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+        meta_keys=(
+            'img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'
+        )
+    )
 ]
+
+# 학습 데이터로더 설정
 train_dataloader = dict(
     batch_size=2,
     num_workers=4,
@@ -228,6 +285,8 @@ train_dataloader = dict(
         backend_args=backend_args
     )
 )
+
+# 검증 데이터로더 설정
 val_dataloader = dict(
     batch_size=2,
     num_workers=4,
@@ -245,6 +304,8 @@ val_dataloader = dict(
         backend_args=backend_args
     )
 )
+
+# 테스트 데이터로더 설정
 test_dataloader = dict(
     batch_size=1,
     num_workers=8,
@@ -262,6 +323,8 @@ test_dataloader = dict(
         backend_args=backend_args
     )
 )
+
+# 검증 평가자 설정
 val_evaluator = dict(
     type='CocoMetric',
     ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/val.json',
@@ -270,6 +333,8 @@ val_evaluator = dict(
     classwise=True,
     backend_args=backend_args
 )
+
+# 테스트 평가자 설정
 test_evaluator = dict(
     type='CocoMetric',
     ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/test.json',
--- baseline/mmdetection/custom_config/DeformableDETR.py
+++ baseline/mmdetection/custom_config/DeformableDETR.py
@@ -1,159 +1,196 @@
-#### model config ####
-# 사용할 model config 주소 상대 주소로 넣기
+#### 모델 설정 ####
+# 사용할 모델 구성 파일의 상대 경로를 설정합니다.
 _base_ = [
     '../configs/deformable_detr/deformable-detr-refine_r50_16xb2-50e_coco.py'
 ]
 
+#### 후크 설정 ####
+# 시각화를 위한 후크 설정
+default_hooks = dict(
+    visualization=dict(
+        type="DetVisualizationHook",
+        draw=True  # 시각화 그림 그리기 활성화
+    )
+)
 
+# 커스텀 후크 설정
+custom_hooks = [
+    dict(type='SubmissionHook')  # 제출을 위한 후크
+]
 
-#### hooks ####
-# hook for visualization
-default_hooks = dict(visualization=dict(type="DetVisualizationHook",draw=True))
-
-# custom hooks
-custom_hooks = [dict(type='SubmissionHook')]
-
-
-
-#### model ####
-# num_classes = 10
-# loss -> cls = FocalLoss, bbox = L1Loss, iou = GIoULoss
+#### 모델 세부 설정 ####
+# 클래스 수 = 10
+# 손실 함수 설정: 분류는 FocalLoss, 바운딩 박스는 L1Loss, IoU는 GIoULoss
 model = dict(
     bbox_head=dict(
-        num_classes=10,
+        num_classes=10,  # 클래스 수 설정
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
-            loss_weight=2.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
-        loss_iou=dict(type='GIoULoss', loss_weight=2.0)
-        ))
+            loss_weight=2.0  # 분류 손실 가중치
+        ),
+        loss_bbox=dict(
+            type='L1Loss',
+            loss_weight=5.0  # 바운딩 박스 손실 가중치
+        ),
+        loss_iou=dict(
+            type='GIoULoss',
+            loss_weight=2.0  # IoU 손실 가중치
+        )
+    )
+)
 
-
-
-#### img size ####
-# img size = (1024, 1024)
+#### 이미지 크기 설정 ####
+# 이미지 크기 = (1024, 1024)
 train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
-    dict(type='PackDetInputs')
+    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),  # 이미지 파일 로드
+    dict(type='LoadAnnotations', with_bbox=True),  # 어노테이션 로드 (바운딩 박스 포함)
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),  # 리사이즈 (비율 유지)
+    dict(type='PackDetInputs')  # 입력 데이터 패킹
 ]
+
 test_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
-    dict(type='PackDetInputs')
+    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),  # 이미지 파일 로드
+    dict(type='LoadAnnotations', with_bbox=True),  # 어노테이션 로드 (바운딩 박스 포함)
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),  # 리사이즈 (비율 유지)
+    dict(type='PackDetInputs')  # 입력 데이터 패킹
 ]
 
-
-
-#### learning policy ####
-# epochs = 12
+#### 학습 정책 설정 ####
+# 총 에폭 수 = 12
 max_epochs = 12
 train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+    type='EpochBasedTrainLoop',
+    max_epochs=max_epochs,      # 총 학습 에폭 수
+    val_interval=1              # 검증 주기 (에폭 단위)
+)
+val_cfg = dict(type='ValLoop')  # 검증 루프 설정
+test_cfg = dict(type='TestLoop')  # 테스트 루프 설정
 
+# 학습률 스케줄러 설정 (MultistepLR 사용)
 param_scheduler = [
     dict(
         type='MultiStepLR',
         begin=0,
         end=max_epochs,
         by_epoch=True,
-        milestones=[40],
-        gamma=0.1)
+        milestones=[40],  # 학습률을 감소시킬 에폭 지점
+        gamma=0.1         # 학습률 감소 비율
+    )
 ]
 
-
-
-#### optimizer ####
-# optimizer = AdamW
-# optim_wrapper = dict(
-#     optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.0001),
-#     )
-# optimizer
+#### 옵티마이저 설정 ####
+# 옵티마이저 = AdamW
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=0.0002, weight_decay=0.0001))
+    optimizer=dict(
+        type='AdamW',
+        lr=0.0002,          # 학습률 설정
+        weight_decay=0.0001 # 가중치 감소 설정
+    )
+)
 
+# 추가적인 옵티마이저 설정 (중복 정의됨)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW',
+        lr=0.0002,          # 학습률 설정
+        weight_decay=0.0001 # 가중치 감소 설정
+    )
+)
 
-#### dataset ####
+#### 데이터셋 설정 ####
 data_root = '/data/ephemeral/home/level2-objectdetection-cv-12/FOLD'
 
 metainfo = {
-    'classes': ('General trash', 'Paper', 'Paper pack', 'Metal', 'Glass',
-                'Plastic', 'Styrofoam', 'Plastic bag', 'Battery', 'Clothing',),
-    'palette': [
+    'classes': (
+        'General trash', 'Paper', 'Paper pack', 'Metal', 'Glass',
+        'Plastic', 'Styrofoam', 'Plastic bag', 'Battery', 'Clothing',
+    ),
+    'palette': [  # 클래스별 색상 설정
         (220, 20, 60), (119, 11, 32), (0, 0, 230), (106, 0, 228), (60, 20, 220),
         (0, 80, 100), (0, 0, 70), (50, 0, 192), (250, 170, 30), (255, 0, 0)
     ]
 }
 
+# 학습 데이터로더 설정
 train_dataloader = dict(
-    batch_size=4,
-    num_workers=5,
+    batch_size=4,  # 배치 사이즈
+    num_workers=5,  # 워커 수
     dataset=dict(
-        data_root=data_root,
-        metainfo=metainfo,
-        ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/train.json',
-        data_prefix=dict(img=''),
-        pipeline=train_pipeline))
+        data_root=data_root,  # 데이터 루트 경로
+        metainfo=metainfo,    # 메타 정보 (클래스 및 색상)
+        ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/train.json',  # 학습 어노테이션 파일
+        data_prefix=dict(img=''),  # 이미지 데이터 경로
+        pipeline=train_pipeline  # 학습 파이프라인
+    )
+)
 
+# 검증 데이터로더 설정
 val_dataloader = dict(
-    batch_size=1,
-    num_workers=5,
+    batch_size=1,  # 배치 사이즈
+    num_workers=5,  # 워커 수
     dataset=dict(
-        data_root=data_root,
-        metainfo=metainfo,
-        ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/val.json',
-        data_prefix=dict(img=''),
-        pipeline=test_pipeline))
+        data_root=data_root,  # 데이터 루트 경로
+        metainfo=metainfo,    # 메타 정보 (클래스 및 색상)
+        ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/val.json',  # 검증 어노테이션 파일
+        data_prefix=dict(img=''),  # 이미지 데이터 경로
+        test_mode=True,  # 테스트 모드 활성화
+        pipeline=test_pipeline  # 검증 파이프라인
+    )
+)
 
+# 테스트 데이터로더 설정
 test_dataloader = dict(
     dataset=dict(
-        data_root=data_root,
-        metainfo=metainfo,
-        ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/test.json',
-        data_prefix=dict(img=''),
-        pipeline=test_pipeline))
+        data_root=data_root,  # 데이터 루트 경로
+        metainfo=metainfo,    # 메타 정보 (클래스 및 색상)
+        ann_file='test.json',  # 테스트 어노테이션 파일
+        data_prefix=dict(img=''),  # 이미지 데이터 경로
+        test_mode=True,  # 테스트 모드 활성화
+        pipeline=test_pipeline  # 테스트 파이프라인
+    )
+)
 
-
-
-#### evaluation ####
+#### 평가자 설정 ####
+# 검증 평가자 설정
 val_evaluator = dict(
     type='CocoMetric',
-    ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/val.json',
-    metric='bbox',
-    format_only=False,
-    classwise=True,
-    )
+    ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/val.json',  # 검증 어노테이션 파일
+    metric='bbox',  # 평가 메트릭 설정 (바운딩 박스)
+    format_only=False,  # 포맷만 출력하지 않음
+    classwise=True,     # 클래스별 평가
+)
 
-test_evaluator = dict(ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/test.json')
+# 테스트 평가자 설정
+test_evaluator = dict(
+    ann_file='/data/ephemeral/home/level2-objectdetection-cv-12/FOLD/test.json',  # 테스트 어노테이션 파일
+)
 
-
-
+#### WandB 설정 ####
 ### wandb ###
 vis_backends = [
-    dict(type='LocalVisBackend'),
-    dict(type='WandbVisBackend',
-         init_kwargs={
-            'project': 'Deformable_DETR',  # Updated project name
-            'entity': 'yujihwan-yonsei-university',
-            'name': 'deformable_DETR_15EPOCH'  # Updated experiment name
-         })]
-
+    dict(type='LocalVisBackend'),  # 로컬 시각화 백엔드
+    dict(
+        type='WandbVisBackend',  # WandB 시각화 백엔드
+        init_kwargs={
+            'project': 'Deformable_DETR',  # 프로젝트 이름 업데이트
+            'entity': 'yujihwan-yonsei-university',  # 엔티티 설정
+            'name': 'deformable_DETR_15EPOCH'  # 실험 이름 업데이트
+        }
+    )
+]
 
 visualizer = dict(
     type='DetLocalVisualizer',
     vis_backends=vis_backends,
-    name='visualizer')
+    name='visualizer'  # 시각화 도구 이름 설정
+)
 
+#### 사전 학습된 모델 로드 ####
+load_from = "https://download.openmmlab.com/mmdetection/v3.0/deformable_detr/deformable-detr-refine_r50_16xb2-50e_coco/deformable-detr-refine_r50_16xb2-50e_coco_20221022_225303-844e0f93.pth"  # 사전 학습된 모델 경로
 
-
-#### pretrained model ####
-load_from = "https://download.openmmlab.com/mmdetection/v3.0/deformable_detr/deformable-detr-refine_r50_16xb2-50e_coco/deformable-detr-refine_r50_16xb2-50e_coco_20221022_225303-844e0f93.pth" 
 
